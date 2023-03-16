538c980063f399f4a4f303d06c7eec5a3a66af1b
Darshil <ldarshill@gmail.com>
2023-03-16 15:17:25 +0530

    add link in variable

--- src/components/Variable/Variable.js
+++ src/components/Variable/Variable.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Variable(props) {
-  return <p> {props.data.text}</p>;
+  const { text, variable } = props.data;
+  let displayText = text;
+
+  Object.entries(variable).forEach(([key, value]) => {
+    const variableValue = value.values ? value.values[0] : value.default_value;
+    const linkTag = `<a href="#">${variableValue}</a>`;
+    displayText = displayText.replace(key, linkTag);
+  });
+
+  return <p dangerouslySetInnerHTML={{ __html: displayText }} />;
 }
