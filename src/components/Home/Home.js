abf998a42164b26d99dbce9c44d6444e8daf4afd
Darshil <ldarshill@gmail.com>
2023-03-16 12:15:21 +0530

    add links in home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default function Home(props) {
+  const links = [
+    "/top_gainers",
+    "/intraday_buying",
+    "/open_high",
+    "/cci_reversal",
+    "/rsi_overbought",
+  ];
+
   return (
     <ul className="home">
-      {props.data.map((data) => (
+      {props.data.map((data, index) => (
         <li key={data.id}>
-          <p> {data.name} </p>
-          <small className={data.color === "green" ? "green" : "red"}>
-            {data.tag}
-          </small>
+          <Link to={links[index]}>
+            <p> {data.name} </p>
+            <small className={data.color === "green" ? "green" : "red"}>
+              {data.tag}
+            </small>
+          </Link>
           <hr />
         </li>
       ))}
